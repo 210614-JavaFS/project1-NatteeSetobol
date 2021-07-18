6402f73e4a021cd5fa62e9f350023532de472635
Natte Setobol <nsetobol@yahoo.com>
2021-07-17 21:32:25 -0700

    Finished the login functionality. Need to research on spring tools and sessions

--- src/main/java/com/revature/controllers/AppController.java
+++ src/main/java/com/revature/controllers/AppController.java
@@ -5,7 +5,11 @@ import java.util.Iterator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.revature.models.ers_user;
 import com.revature.services.UserServices;
@@ -19,11 +23,38 @@ public class AppController {
 	@RequestMapping("/")
 	public String viewHomePage()
 	{	
+		return "index.jsp";
+	}
 	
+	@RequestMapping("/login")
+	public ModelAndView login()
+	{
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("error", "Username and/or Password is wrong!");
+		modelAndView.setViewName("login.jsp");
+		
+		return modelAndView;
+	}
+	
+	@PostMapping("/dologin")
+	public ModelAndView doLogin(@RequestParam("username") String username, @RequestParam("password") String password )
+	{
+		ModelAndView modelAndView = null;
 		ArrayList<ers_user> usersInDatabase = userService.getUserByUsername("test");
 		
-		System.out.println("There are " + usersInDatabase.size());
-		return "index.jsp";
+		for (int usersInDatabaseIndex = 0; usersInDatabaseIndex < usersInDatabase.size();usersInDatabaseIndex++)
+		{
+			ers_user user = usersInDatabase.get(usersInDatabaseIndex);
+			
+			if (user.getErs_username().equals(username) && user.getErs_password().equals(password))
+			{
+				modelAndView = new ModelAndView("redirect:/");
+				return modelAndView;
+			}
+		}
+		
+		modelAndView = new ModelAndView("redirect:/login?error=\"yes\"");
+		return modelAndView;
 	}
 	
 	@RequestMapping("/index")
