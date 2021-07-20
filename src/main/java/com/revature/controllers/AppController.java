fe58852bea52ea88f98a000958b5536ffb3892db
Natte Setobol <nsetobol@yahoo.com>
2021-07-19 21:38:06 -0700

    Added httpsessions

--- src/main/java/com/revature/controllers/AppController.java
+++ src/main/java/com/revature/controllers/AppController.java
@@ -3,6 +3,9 @@ package com.revature.controllers;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,52 +17,97 @@ import org.springframework.web.servlet.ModelAndView;
 import com.revature.models.ers_user;
 import com.revature.services.UserServices;
 
+import antlr.collections.List;
+
 @Controller
 public class AppController {
 	
 	@Autowired 
 	UserServices userService;
+	//UserController userController  = null;
+	
+	AppController()
+	{
+	}
 	
 	@RequestMapping("/")
-	public String viewHomePage()
+	public ModelAndView viewHomePage(HttpSession httpSession )
 	{	
-		return "index.jsp";
+		ModelAndView modelAndView = null; 
+		String redirectString = "redirect:/main";
+		@SuppressWarnings("unchecked")
+		ArrayList<String> messages = (ArrayList<String>) httpSession.getAttribute("SPRING_BOOT_SESSION_MESSAGES");
+		/*
+		 TODO(): This is not a good security measure.
+		 IDEAS:
+		 When user login, store a hash something and put it in the database and session array, then in the index logic
+		 get each hash in the database and see if it matches a hash in the session array.
+		*/
+		if (messages == null)
+		{
+			redirectString = "redirect:/login";
+			modelAndView = new ModelAndView(redirectString);
+			return modelAndView;
+		} 
+			
+		modelAndView = new ModelAndView();
+		modelAndView.setViewName("index.jsp");
+		
+		
+		return modelAndView;
 	}
 	
 	@RequestMapping("/login")
-	public ModelAndView login()
+	public ModelAndView login(@RequestParam(required = false) String error,HttpSession httpSession )
 	{
+		
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("error", "Username and/or Password is wrong!");
+		if (error != null)
+		{
+			if (error.equals("yes"))
+			{
+				modelAndView.addObject("error", "Username and/or Password is wrong!");
+			}
+		}
 		modelAndView.setViewName("login.jsp");
 		
 		return modelAndView;
 	}
 	
 	@PostMapping("/dologin")
-	public ModelAndView doLogin(@RequestParam("username") String username, @RequestParam("password") String password )
+	public ModelAndView doLogin(@RequestParam("username") String username, @RequestParam("password") String password,  HttpServletRequest httpServletRequest )
 	{
 		ModelAndView modelAndView = null;
-		ArrayList<ers_user> usersInDatabase = userService.getUserByUsername("test");
+		String nextPage = null;
+		ers_user result = null;
+		
+		nextPage = "redirect:/login?error=yes";
+		
+		ArrayList<ers_user> usersInDatabase = userService.getUserByUsername(username);
 		
 		for (int usersInDatabaseIndex = 0; usersInDatabaseIndex < usersInDatabase.size();usersInDatabaseIndex++)
 		{
-			ers_user user = usersInDatabase.get(usersInDatabaseIndex);
+			result = usersInDatabase.get(usersInDatabaseIndex);
 			
-			if (user.getErs_username().equals(username) && user.getErs_password().equals(password))
+			if (result.getErs_username().equals(username) && result.getErs_password().equals(password))
 			{
-				modelAndView = new ModelAndView("redirect:/");
-				return modelAndView;
+				@SuppressWarnings("unchecked")
+				ArrayList<ers_user>  session = (ArrayList<ers_user>) httpServletRequest.getSession().getAttribute("SPRING_BOOT_SESSION_MESSAGES");
+				if (session == null)
+				{
+					session = new ArrayList<>();
+					httpServletRequest.getSession().setAttribute("SPRING_BOOT_SESSION_MESSAGES", session);
+					session.add(result);
+					httpServletRequest.getSession().setAttribute("SPRING_BOOT_SESSION_MESSAGES", session);
+				}
+				nextPage = "redirect:/";
+				break;
 			}
 		}
 		
-		modelAndView = new ModelAndView("redirect:/login?error=\"yes\"");
+		modelAndView = new ModelAndView(nextPage);
 		return modelAndView;
 	}
 	
-	@RequestMapping("/index")
-	public String index()
-	{
-		return "index";
-	}
+
 }
--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,18 +1,22 @@
 package com.revature.controllers;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.revature.models.ers_user;
+import com.revature.services.UserServices;
 
 @Controller
 public class UserController {
+	
 
 	
-	public ers_user Login(String username, String userPassword)
+	public ers_user userLogin(String username, String userPassword)
 	{
 		ers_user result = null;
-		
 		
 
 		return result;
