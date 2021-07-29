2dd0bbd84417b6a9989c1333150ca409b6f5b1ee
Natte Setobol <nsetobol@yahoo.com>
2021-07-29 06:45:18 -0700

    Added password encryption

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,7 +1,14 @@
 package com.revature.controllers;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Base64;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +22,8 @@ import com.revature.models.ers_user;
 import com.revature.services.ReimbursementService;
 import com.revature.services.ReimbursementTypeServices;
 import com.revature.services.RembursmentStatusServices;
-import com.revature.services.UserRoleServices;
 import com.revature.services.UserServices;
+import com.revature.utils.aes;
 
 @Controller
 public class UserController {
@@ -31,6 +38,8 @@ public class UserController {
 
 	@Autowired
 	ReimbursementService reimbursementService;
+	
+
 	
 	@PostMapping("/doReimbursment")
 	@ResponseBody
@@ -69,6 +78,7 @@ public class UserController {
 		ModelAndView modelAndView = null;
 		String nextPage = null;
 		ers_user result = null;
+		aes encryption = null;
 		
 		nextPage = "redirect:/login?error=yes";
 		
@@ -77,8 +87,9 @@ public class UserController {
 		for (int usersInDatabaseIndex = 0; usersInDatabaseIndex < usersInDatabase.size();usersInDatabaseIndex++)
 		{
 			result = usersInDatabase.get(usersInDatabaseIndex);
-			
-			if (result.getErs_username().equals(username) && result.getErs_password().equals(password))
+			encryption = new aes();
+			// TODO(): Find a better way to hide the secret key. Leaving the secret key out like this is very unsecured.
+			if (result.getErs_username().equals(username) && result.getErs_password().equals(encryption.encrypt(password, "secertkey")))
 			{
 				@SuppressWarnings("unchecked")
 				ArrayList<ers_user>  session = (ArrayList<ers_user>) httpServletRequest.getSession().getAttribute("SPRING_BOOT_SESSION_MESSAGES");
@@ -97,5 +108,8 @@ public class UserController {
 		modelAndView = new ModelAndView(nextPage);
 		return modelAndView;
 	}
+	
+
+	
 	
 }
--- src/main/webapp/scripts/login.js
+++ src/main/webapp/scripts/login.js
@@ -2,3 +2,33 @@
  * 
  */
 
+function checkLogin()
+{
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", "/dologin", true);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	
+ 	xhr.onreadystatechange = function ()
+ 	{
+    	if(this.readyState===4 && this.status===200)
+    	{
+      		let data = JSON.parse(xhr.responseText);
+
+			if (data)
+			{
+				if (data.error)
+				{
+					alert(data.error);
+				} else {
+					console.log(data);
+					for (ticketIndex = 0; ticketIndex < data.length; ticketIndex++)
+					{
+						AddToTicketHistoryTable(ticketIndex,data[ticketIndex]);
+					}
+				}
+			}
+		}
+    }
+
+	xhr.send();
+}
